fcbd63c4bd48d8d007df5ba13519fadaba557aee
amraw <amitrawat10@gmail.com>
2017-04-02 19:14:44 +0530

    Graph file

---
+++ data_structures/JAVA_WORKSPACE/Algorithms_DataStructure/src/graphs/Graph.java
@@ -0,0 +1,18 @@
+package graphs;
+
+public class Graph {
+	int N;
+	int V;
+	
+	Graph(int N, int V)
+	{
+		this.N=N;
+		this.V=V;
+	}
+	
+	public static void main(String []args)
+	{
+		System.out.println("Hello world");
+	}
+
+}
